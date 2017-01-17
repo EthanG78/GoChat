2ba20606e7b3e87a15915559267b498e4621e419
Ciisco <EthanBB13@gmail.com>
2017-01-16 20:41:19 -0400

    Added animated lassajous at /lassajous

--- lib/connection.go
+++ lib/connection.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/websocket"
 	"net/http"
 	"net"
+
 )
 
 //////////////////////
--- main.go
+++ main.go
@@ -8,8 +8,13 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"github.com/EthanG78/golang_chat/lib"
 	"github.com/satori/go.uuid"
-
+	"math/rand"
 	"time"
+	"io"
+	"image/color"
+	"image/gif"
+	"image"
+	"math"
 )
 
 
@@ -21,6 +26,9 @@ type user struct {
 	UserName string
 	Pass     string
 }
+
+
+
 
 var dbUsers = map[string]user{}
 var dbSessions = map[string]string{}
@@ -154,7 +162,46 @@ func faviconHandler(w http.ResponseWriter, r *http.Request){
 	http.ServeFile(w, r, "styling/logo/favicon.ico")
 }
 
+//SECRET ANIMATED GIF
+var palette = []color.Color{color.White, color.Black}
 
+const(
+	whiteIndex = 0 //first color in palette
+	blackIndex = 1 //next color in palette
+)
+
+func lassajous(out io.Writer)  {
+	const(
+		cycles = 5 //number of complex x oscillator revolutions
+		res = 0.001 // angular resolution
+		size = 100 //image canvas covers [-size..+size]
+		nframes = 64 //number of animation frames
+		delay = 8 // delay between frames in 10ms units
+
+	)
+
+	freq := rand.Float64()* 3.0
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0,0,2*size+1,2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res{
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // ignoring encoding errors.
+
+}
+
+func lassajousHandler (w http.ResponseWriter, r *http.Request){
+	lassajous(w)
+}
 func main() {
 
 	flag.Parse()
@@ -165,6 +212,7 @@ func main() {
 	router.HandleFunc("/", sign_up)
 	router.HandleFunc("/login", login)
 	router.HandleFunc("/forbidden", forbidden)
+	router.HandleFunc("/lassajous", lassajousHandler)
 	router.Handle("/chat", homeHandler(tpl))
 	router.Handle("/ws", lib.WsHandler{H:H})
 	log.Println("serving on port 8080")
