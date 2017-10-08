5a7fbb80e3f4a3d7a888f0095fc2faa710839bb8
Ciisco <EthanBB13@gmail.com>
2017-10-08 15:24:25 -0300

    Added admin endpoint

--- main.go
+++ main.go
@@ -6,53 +6,54 @@ import (
 	"net/http"
 	"text/template"
 	"time"
-	mw "github.com/EthanG78/golang_chat/middleware"
+
 	"github.com/EthanG78/golang_chat/lib"
-	"golang.org/x/crypto/bcrypt"
+	mw "github.com/EthanG78/golang_chat/middleware"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
 //Ethan Zaat is a cool dude;)
 
+//Default User type
 type User struct {
-	Username 		string		`json:"username"`
-	Pass     		[]byte		`json:"pass"`
+	Username string `json:"username"`
+	Pass     []byte `json:"pass"`
 }
 
 var dbUsers = map[string]User{}
 var dbSessions = map[string]string{}
 
-func home (c echo.Context) error{
+func home(c echo.Context) error {
 	return c.String(http.StatusOK, "home")
 }
 
-func four_o_one (c echo.Context) error{
+func four0one(c echo.Context) error {
 	return c.String(http.StatusUnauthorized, "Nice try buster, you are unauthorized!")
 }
 
-func sign_up (c echo.Context) error{
+func signUp(c echo.Context) error {
 	var u User
-	if c.Request().Method == http.MethodPost{
+	if c.Request().Method == http.MethodPost {
 		un := c.Request().FormValue("username")
 		p := c.Request().FormValue("password")
-
 
 		//TODO: Make an individual way of handling when users do not insert anything into the fields..
 		if un == "" {
 			time.Sleep(3000)
 			RedirectError := c.Redirect(http.StatusFound, "/401")
 			//Error checking for testing
-			if RedirectError != nil{
+			if RedirectError != nil {
 				log.Printf("Error: %v", RedirectError)
 			}
 		}
-		if p == ""{
+		if p == "" {
 			time.Sleep(3000)
-			RedirectError:= c.Redirect(http.StatusFound, "/401")
+			RedirectError := c.Redirect(http.StatusFound, "/401")
 			//Error checking for testing
-			if RedirectError != nil{
+			if RedirectError != nil {
 				log.Printf("Error: %v", RedirectError)
 			}
 		}
@@ -68,9 +69,8 @@ func sign_up (c echo.Context) error{
 
 		dbSessions[cookie.Value] = un
 
-
 		finalP, err := bcrypt.GenerateFromPassword([]byte(p), 0)
-		if err != nil{
+		if err != nil {
 			log.Fatalf("Error encrypting password: %v", err)
 			//This is probably really bad, should find a better way to handle it lmao
 		}
@@ -80,10 +80,9 @@ func sign_up (c echo.Context) error{
 		dbUsers[un] = u
 		RedirectError := c.Redirect(http.StatusFound, "/login")
 		//Error checking for testing
-		if RedirectError != nil{
+		if RedirectError != nil {
 			log.Printf("Error: %v", RedirectError)
 		}
-
 
 		//FOR DEBUGGING
 		log.Println(dbUsers)
@@ -93,13 +92,12 @@ func sign_up (c echo.Context) error{
 
 	}
 
-
 	return c.String(http.StatusBadRequest, "You could not be signed up")
 
 }
 
-func login (c echo.Context) error{
-	if c.Request().Method == http.MethodPost{
+func login(c echo.Context) error {
+	if c.Request().Method == http.MethodPost {
 		un := c.Request().FormValue("username")
 		p := c.Request().FormValue("password")
 
@@ -107,7 +105,7 @@ func login (c echo.Context) error{
 			time.Sleep(3000)
 			RedirectError := c.Redirect(http.StatusFound, "/401")
 			//Error checking for testing
-			if RedirectError != nil{
+			if RedirectError != nil {
 				log.Printf("Error: %v", RedirectError)
 			}
 		}
@@ -115,7 +113,7 @@ func login (c echo.Context) error{
 			time.Sleep(3000)
 			RedirectError := c.Redirect(http.StatusFound, "/401")
 			//Error checking for testing
-			if RedirectError != nil{
+			if RedirectError != nil {
 				log.Printf("Error: %v", RedirectError)
 			}
 		}
@@ -125,18 +123,18 @@ func login (c echo.Context) error{
 			time.Sleep(3000)
 			RedirectError := c.Redirect(http.StatusFound, "/401")
 			//Error checking for testing
-			if RedirectError != nil{
+			if RedirectError != nil {
 				log.Printf("Error: %v", RedirectError)
 			}
 		}
 
 		err := bcrypt.CompareHashAndPassword(u.Pass, []byte(p))
-		if err != nil{
+		if err != nil {
 			time.Sleep(3000)
 			RedirectError := c.Redirect(http.StatusFound, "/401")
 			//This is in the case that they input the incorrect password
 			//Error checking for testing
-			if RedirectError != nil{
+			if RedirectError != nil {
 				log.Printf("Error: %v", RedirectError)
 			}
 		}
@@ -149,13 +147,11 @@ func login (c echo.Context) error{
 
 		dbSessions[cookie.Value] = un
 
-		RedirectError := c.Redirect(http.StatusFound, "/chat")
+		RedirectError := c.Redirect(http.StatusFound, "/chat/")
 		//Error checking for testing
-		if RedirectError != nil{
+		if RedirectError != nil {
 			log.Printf("Error: %v", RedirectError)
 		}
-
-		log.Printf("SERVER: User '%v' has logged in\n", u.Username )
 		return c.String(http.StatusOK, "You have successfully logged in!")
 
 	}
@@ -163,14 +159,15 @@ func login (c echo.Context) error{
 	return c.String(http.StatusBadRequest, "You could not log in")
 }
 
-func chat (c echo.Context) error{
+func chatMain(c echo.Context) error {
 	tpl := template.Must(template.ParseGlob("static/chat.html"))
 	tpl.Execute(c.Response(), c.Request())
 	return c.String(http.StatusOK, "")
 }
 
-
-
+func adminMain(c echo.Context) error {
+	return c.String(http.StatusOK, "Welcome to the admin page.")
+}
 
 //////
 //MAIN
@@ -179,7 +176,6 @@ func main() {
 
 	e := echo.New()
 	flag.Parse()
-
 
 	e.File("/favicon.ico", "static/styling/favicon.ico")
 
@@ -190,24 +186,29 @@ func main() {
 
 	//TODO: ADD SESSION COOKIES, so no random dude can access chat without logging in
 
-
 	//TODO: I also have to use echo's websockets... That's going to be brutal
 
 	//TODO: Create admin group and page
 
-
 	//GROUPS
 	admin := e.Group("/admin")
-
+	chat := e.Group("/chat")
 
 	//MIDDLEWARE
+	e.Use(mw.ServerInfo)
+
+	chat.Use(mw.CheckCookies)
+
+	chat.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency}` + "\n",
+	}))
+
 	admin.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency}` + "\n",
 	}))
 
-	admin.Use(middleware.BasicAuth(func (username, password string, c echo.Context) (bool, error) {
-		//placeholders for now
-		if username == mw.AdminLogin && password == mw.AdminPassword{
+	admin.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == mw.AdminLogin && password == mw.AdminPassword {
 			return true, nil
 		}
 
@@ -220,18 +221,18 @@ func main() {
 	//ENDPOINTS
 	e.GET("/", home)
 	e.File("/", "static/home.html")
-	e.GET("/401", four_o_one)
+	e.GET("/401", four0one)
 	e.File("/401", "static/forbidden.html")
-	e.POST("/signup", sign_up)
+	e.POST("/signup", signUp)
 	e.File("/signup", "static/signup.html")
 	e.POST("/login", login)
 	e.File("/login", "static/login.html")
-	e.GET("/chat", chat)
+	chat.GET("/", chatMain)
+	admin.GET("/", adminMain)
+	//admin.File("/", "static/admin.html")
 
 	//CREATE SERVER
 	e.Logger.Fatal(e.Start(":8080"))
-
-
 
 }
 
--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,35 +1,35 @@
 package middleware
 
 import (
-	"github.com/labstack/echo"
-	"strings"
-	"net/http"
 	"log"
+	"net/http"
+	"strings"
+
+	"github.com/labstack/echo"
 )
 
-func ServerInfo(pipe echo.HandlerFunc) echo.HandlerFunc{
-	return func (c echo.Context) error{
+func ServerInfo(pipe echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
 		c.Response().Header().Set(echo.HeaderServer, "GoChat/1.0")
 		c.Response().Header().Set("Author", "Ethan Garnier")
 		return pipe(c)
 	}
 }
 
-func checkCookies (pipe echo.HandlerFunc) echo.HandlerFunc{
-	return func (c echo.Context) error{
+func CheckCookies(pipe echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
 		cookie, err := c.Cookie("session_id")
-		if err != nil{
-			if strings.Contains(err.Error(), "named cookie not present"){
+		if err != nil {
+			if strings.Contains(err.Error(), "named cookie not present") {
 				return c.String(http.StatusUnauthorized, "Seems like the cookie monster was here..\n You do not have any cookies")
 			}
 
 			log.Printf("Error: %v \n", err)
 			return err
 
-
 		}
 
-		if cookie.Name == "session_id"{
+		if cookie.Name == "session_id" {
 			return pipe(c)
 		}
 
